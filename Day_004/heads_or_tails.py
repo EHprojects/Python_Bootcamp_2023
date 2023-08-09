865b218629504ab75651bd053677108623ca80d5
Eric H <elharvey234@gmail.com>
2023-08-09 19:08:10 -0400

    Initial Commit

---
+++ Day_004/heads_or_tails.py
@@ -0,0 +1,11 @@
+# Remember to use the random module
+# Hint: Remember to import the random module here at the top of the file. 🎲
+
+# Write the rest of your code below this line 👇
+import random
+
+rand_num = random.randint(0, 1)
+if rand_num == 1:
+    print("Heads")
+else:
+    print("Tails")
