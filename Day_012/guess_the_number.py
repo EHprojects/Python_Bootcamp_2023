e1e1648b8ed0890b1ca71df0cbc8b70f71dfd9ef
Eric H <elharvey234@gmail.com>
2023-08-17 19:37:46 -0400

    Initial Commit

--- Day_012/guess_the_number.py
+++ Day_012/guess_the_number.py
@@ -14,47 +14,50 @@
 from art import logo
 import random
 
-print(logo)
-print("Welcome to the Number Guessing Game!")
-
-secret_num = random.randint(1, 100)
-print("I'm thinking of a number between 1 and 100.")
-print(f"Pssst, the correct answer is {secret_num}")
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-
-num_guesses = 0
-if difficulty == "easy":
-    num_guesses = 10
-else:
-    num_guesses = 5
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 
 
-def check_guess(user_guess, secret_num):
-    pass
-
-
-game_over = False
-# repeat from here
-
-while not game_over:
-    print(f"You have {num_guesses} attempts remaining to guess the number.")
-
-    user_guess = int(input("Make a guess: "))
-
+def check_guess(user_guess, secret_num, turns):
     if user_guess == secret_num:
         print(f"You got it! The answer was {secret_num}.")
     elif user_guess < secret_num:
         print(f"Too low.")
-        num_guesses -= 1
+        return turns - 1
     else:
         print("Too high.")
-        num_guesses -= 1
+        return turns - 1
 
-    if num_guesses > 0:
-        print("Guess again.")
+
+def set_difficulty():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if difficulty == "easy":
+        return EASY_LEVEL_TURNS
     else:
-        print("You've run out of guesses, you lose.")
-        game_over = True
+        return HARD_LEVEL_TURNS
 
 
+def game():
+    print(logo)
+    print("Welcome to the Number Guessing Game!")
 
+    secret_num = random.randint(1, 100)
+    print("I'm thinking of a number between 1 and 100.")
+    print(f"Pssst, the correct answer is {secret_num}")
+
+    turns = set_difficulty()
+
+    user_guess = 0
+    while user_guess != secret_num:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        user_guess = int(input("Make a guess: "))
+        turns = check_guess(user_guess, secret_num, turns)
+
+        if turns == 0:
+            print("You've run out of guesses, you lose.")
+            return
+        elif user_guess != secret_num:
+            print("Guess again.")
+
+
+game()
