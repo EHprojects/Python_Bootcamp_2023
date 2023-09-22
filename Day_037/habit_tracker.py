765f6954a243bc4fdec502824db3a172e8bec170
Eric H <elharvey234@gmail.com>
2023-09-21 20:30:38 -0400

    Updates

--- Day_037/habit_tracker.py
+++ Day_037/habit_tracker.py
@@ -8,6 +8,7 @@ load_dotenv("../.idea/.env")
 # Pixela API Info
 PIX_API_TOKEN = os.getenv("PIX_API_TOKEN")
 PIX_USER_NAME = os.getenv("PIX_USER_NAME")
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -30,8 +31,27 @@ headers = {
 
 pixel_post_endpoint = f"{graph_endpoint}/graph1"
 
+yesterday = datetime(year=2023, month=9, day=20)
+yesterday = yesterday.strftime("%Y%m%d")
+
 pixel_post_params = {
-    "date": "20230920",
-    "quantity": "22"
+    "date": yesterday,
+    "quantity": "20"
 }
 
+# response = requests.post(url=pixel_post_endpoint, json=pixel_post_params, headers=headers)
+# print(response.text)
+
+pixel_update_params = {
+    "quantity": "24"
+}
+
+pixel_update_end = f"{pixela_endpoint}/{PIX_USER_NAME}/graphs/{GRAPH_ID}/{yesterday}"
+
+# Update
+# response = requests.put(url=pixel_update_end, json=pixel_update_params, headers=headers)
+# print(response.text)
+
+# Delete
+response = requests.delete(url=pixel_update_end, headers=headers)
+print(response.text)
