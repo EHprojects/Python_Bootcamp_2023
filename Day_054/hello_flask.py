b55750296597ddbfebafd1caaef6524fc6fd4f6b
EHprojects <elharvey234@gmail.com>
2023-10-29 13:08:39 -0400

    Updates

--- Day_054/hello_flask.py
+++ Day_054/hello_flask.py
@@ -1,7 +1,9 @@
 from flask import Flask
-# from simple_dotenv import GetEnv
+from simple_dotenv import GetEnv
+# from dotenv import load_dotenv
 
-# env = GetEnv('ENV_VARIABLE_KEY')
+
+env = GetEnv('ENV_VARIABLE_KEY')
 
 
 app = Flask(__name__)
@@ -10,3 +12,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run()
