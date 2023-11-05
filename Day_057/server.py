9a3f90a5ce9d3c914a09d3ab0b4c54e7b6208630
EHprojects <elharvey234@gmail.com>
2023-11-05 12:25:49 -0500

    Updates

--- Day_057/server.py
+++ Day_057/server.py
@@ -35,16 +35,25 @@ def home():
     rand_num = random.randint(1, 10)
     current_year = datetime.now().year
     my_name = "EH Projects"
-    return render_template("index.html", num=rand_num, year=current_year, name=my_name)
+    return render_template("index_old.html", num=rand_num, year=current_year, name=my_name)
 
 
 # Day 33 for API requests
 @app.route("/guess/<name>")
 def guess(name):
-    name = str(name).title()
     age = get_age(name)
     gender = get_gender(name)
+    # name = str(name).title()
     return render_template("guess.html", name=name, gender=gender, age=age)
+
+
+@app.route("/blog/<int:num>")
+def get_blog(num):
+    print(num)
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
 
 
 if __name__ == "__main__":
