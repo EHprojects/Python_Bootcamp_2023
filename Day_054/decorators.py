341adf00145977415c05d9d9f66e72f172cda540
Eric H <elharvey234@gmail.com>
2024-04-13 15:52:07 -0400

    Updates

---
+++ Day_054/decorators.py
@@ -0,0 +1,34 @@
+import time
+
+current_time = time.time()
+print(current_time)  # seconds since Jan 1st, 1970
+
+
+# Write your code below 👇
+
+def speed_calc_decorator(function):
+    def benchmark():
+        start_t = time.time()
+        function()
+        end_t = time.time()
+        total_t = end_t - start_t
+        # print(function.__name__)
+        print(f"{function.__name__} run speed: {total_t}s")
+
+    return benchmark
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(1000000):
+        i * i
+
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(10000000):
+        i * i
+
+
+fast_function()
+slow_function()
